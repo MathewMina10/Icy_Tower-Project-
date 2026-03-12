0ac1dea7f34d79cd866f61f58f77ff67012a6e11
MathewMina10 <20251700637@cis.asu.edu.eg>
2026-03-12 20:18:33 +0200

    Add project files.

---
+++ ConsoleApplication1/Main.cpp
@@ -0,0 +1,34 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <SFML/Audio.hpp>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(1440,900),"sfml works");
+    Event event;
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        Texture t;
+        t.loadFromFile("satoru-gojo-smoking-jujutsu-kaisen-hd-wallpaper-uhdpaper.com-10@2@a.jpg");
+        Sprite s;
+        s.setTexture(t);
+        s.setScale(0.5, 0.5);
+        window.clear();
+        window.draw(s);
+        window.display();
+    }
+    return 0;
+}
